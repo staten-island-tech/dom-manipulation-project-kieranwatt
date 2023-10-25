fd35935084e15d0612a69ed86293dcd9851b34dd
kieranwatt <kieranmwatt@gmail.com>
2023-10-25 09:26:12 -0400

    wednesdayv1

--- object.js
+++ object.js
@@ -1,3 +1,12 @@
 const DomSelectors = {
-    button: document.getElementById(`btn`)
+    button: document.getElementById(`btn`),
+    text: document.querySelector(`#text`),
+    point: document.querySelectorAll(`.point`),
+    pointTwo: doc.getElementById(`pointTwo`),
+};
+
+function backroundAndText(backround, text) {
+    backround.style.backroundColor="black";
 }
+
+DomSelectors.button.addEventListener
