aeb609ceb3018d56b3a516b4c7c16f6e1619b0ee
kieranwatt <kieranmwatt@gmail.com>
2023-10-26 09:26:12 -0400

    thursdayv1

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const DomSelectors = {
     input: document.querySelector(`#input`),
 };
 
-DomSelectors.box.insertAdjacentHTML("beforebegin", `<h1>bladee</h1>`)
+/* DomSelectors.box.insertAdjacentHTML("beforebegin", `<h1>bladee</h1>`)
 DomSelectors.button.addEventListener("click", function () {
     let input = DomSelectors.input.value;
     console.log(input);
@@ -16,21 +16,33 @@ function backroundAndText(background, text) {
     background.style.backroundColor = "red"; //not working+want to change to a backroundimage
     text.innerHTML = "BLADEE";
     text.style.fontSize = "55px";
-}
+} */
 
-/* backroundAndText(DOMSelectors.box, DomSelectors.text); */
 
-DomSelectors.button.addEventListener("click", function() {
-    backroundAndText(DomSelectors.box, DomSelectors.text);
+DomSelectors.button.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const card = {
+        title: DomSelectors.title.value,
+        image: DomSelectors.image.value,
+        description: DomSelectors.description.value,
+    };
+    injectCard(card);
+    clearFields();
 });
 
-function changeLi(){
-    let pointIndex = 1;
-    DomSelectors.points.forEach((point) => {
-        point.addEventListener("click", function () {
-            point.textContent = `Bladee is the best ${pointIndex}`;
-            pointIndex++;
-        });
-    });
+function injectCard(card) {
+    DomSelectors.container.insertAdjacentHTML(
+        "afterbegin",`
+        <div class="bladeecard1">
+        <div class="card">
+        <h2> 
+
+    `)
 }
-changeLi();
+
+
+function clearFields() {
+    DomSelectors.title.value = "";
+    DomSelectors
+    DomSelectors
+};
