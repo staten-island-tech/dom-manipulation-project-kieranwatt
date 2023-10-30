0b5713f735982cfd5cdca43e5a9dd5d7d4600ca6
kieranwatt <kieranmwatt@gmail.com>
2023-10-30 09:14:23 -0400

    yooooooooo

--- main.js
+++ main.js
@@ -1,52 +1,41 @@
 const DomSelectors = {
-    text: document.querySelector("#text"),
-    box: document.getElementById("big-black-box"),
-    points: document.querySelectorAll(".point"),
-    input: document.querySelector(`#input`),
+    form: document.querySelector("#input-form"),
+    box: document.querySelector("box"),
+    bladeename: document.querySelector("namebladee"),
+    bladeebday: document.querySelector("bdaybladee"),
+    bladeeimg: document.querySelector("imagebladee"),
 };
 
-/* DomSelectors.box.insertAdjacentHTML("beforebegin", `<h1>bladee</h1>`)
-DomSelectors.button.addEventListener("click", function () {
-    let input = DomSelectors.input.value;
-    console.log(input);
-})
-
-function backroundAndText(background, text) {
-    background.style.backroundColor = "red"; //not working+want to change to a backroundimage
-    text.innerHTML = "BLADEE";
-    text.style.fontSize = "55px";
-} */
-
-
-DomSelectors.button.addEventListener("submit", function(event) {
+DomSelectors.form.addEventListener("submit", function(event) {
     event.preventDefault();
     const card = {
-        title: DomSelectors.title.value,
-        image: DomSelectors.image.value,
-        description: DomSelectors.description.value,
-    };
+        bladeename: DomSelectors.namebladee.value,
+        bladeeimg: DomSelectors.imagebladee.value,
+        bladeebday: DomSelectors.bdaybladee.value,
+    }
     injectCard(card);
-    clearFields();
+    deletee();
 });
 
 function injectCard(card) {
-    DomSelectors.container.insertAdjacentHTML(
+    DomSelectors.box.insertAdjacentHTML(
         "afterbegin",`
         <div class="bladeecard1">
         <div class="card">
-        <h2> ${card.title}<h2/> 
-        <img class= "card-img" src="${card.image}" alt="">
-        <p> ${card.description}</p>
+        <h1> ${card.title} </h2>
+        <img class="card-img" src="${card.bladeeimg}" alt="">
+        <h3>${card.bladeebday}</p>
         </div>
         <button class="removeBtn" onclick="removeCard()">Remove<button>
-        </div>
-
-    `)
+        </div>`)
 }
 
-
 function clearFields() {
-    DomSelectors.title.value = "";
-    DomSelectors
-    DomSelectors
+    DomSelectors.bladeebday.value = "";
+    DomSelectors.bladeeimg.value = "";
+    DomSelectors.bladeename.value = "";
 };
+
+function deletee() {
+
+}
